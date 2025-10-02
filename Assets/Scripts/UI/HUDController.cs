71f6466913911fc306c8bc502cf5aba226126fee
ExistedAura7234 <162326727+ExistedAura7234@users.noreply.github.com>
2025-10-02 16:20:53 -0300

    Implementación de HUD, armamento y notas del programador (Armamento beta)

---
+++ Assets/Scripts/UI/HUDController.cs
@@ -0,0 +1,17 @@
+// Código del controlador HUD
+
+using UnityEngine;
+
+public class HUDController : MonoBehaviour
+{
+    // Funciones y variables del controlador HUD
+    void Start()
+    {
+        // Inicialización del HUD
+    }
+
+    void Update()
+    {
+        // Actualización del HUD
+    }
+}
