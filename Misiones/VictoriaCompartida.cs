4996416dad9a2e7944528ff7360d7f126ddff012
ExistedAura7234 <162326727+ExistedAura7234@users.noreply.github.com>
2025-10-08 20:43:06 -0300

    Create VictoriaCompartida.cs

---
+++ Misiones/VictoriaCompartida.cs
@@ -0,0 +1,7 @@
+using System;
+
+public class VictoriaCompartida
+{
+    public string Titulo => "Victoria Compartida";
+    public string Descripcion => "El Escuadrón Tesla llega y juntos logran defender el cuartel general de la Resistencia.";
+}
