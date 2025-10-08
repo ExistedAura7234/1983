d26a934d57b1435734e7b58ac85f4b7951e0e394
ExistedAura7234 <162326727+ExistedAura7234@users.noreply.github.com>
2025-10-08 20:42:47 -0300

    Create DefensaDelRefugio.cs

---
+++ Misiones/DefensaDelRefugio.cs
@@ -0,0 +1,7 @@
+using System;
+
+public class DefensaDelRefugio
+{
+    public string Titulo => "Defensa del Refugio";
+    public string Descripcion => "Los ingenieros encuentran una debilidad en el sistema enemigo, pero el cuartel es atacado. Abdullah debe coordinar la defensa con ayuda del Escuadrón Tesla.";
+}
