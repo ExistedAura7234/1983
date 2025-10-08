19cd9749de08cd5b56688512a0b47d0a8f838eff
ExistedAura7234 <162326727+ExistedAura7234@users.noreply.github.com>
2025-10-08 20:39:43 -0300

    Create RoboSolitario.cs

---
+++ Misiones/RoboSolitario.cs
@@ -0,0 +1,7 @@
+using System;
+
+public class RoboSolitario
+{
+    public string Titulo => "Robo Solitario";
+    public string Descripcion => "Para probar su lealtad, Abdullah debe infiltrarse solo en un almacén y robar provisiones necesarias para la Resistencia.";
+}
