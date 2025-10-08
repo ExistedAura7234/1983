0a8d0ad0572861b16405b8db605758aff03877b6
ExistedAura7234 <162326727+ExistedAura7234@users.noreply.github.com>
2025-10-08 20:44:10 -0300

    Create AsaltoFinal.cs

---
+++ Misiones/AsaltoFinal.cs
@@ -0,0 +1,7 @@
+using System;
+
+public class AsaltoFinal
+{
+    public string Titulo => "Asalto Final";
+    public string Descripcion => "Con todo preparado, la Resistencia lanza el ataque final contra el A.H.S. El destino de la ciudad está en juego.";
+}
