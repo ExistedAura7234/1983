3aa27a84540a999e76de324430105a732255989d
ExistedAura7234 <162326727+ExistedAura7234@users.noreply.github.com>
2025-10-08 20:40:36 -0300

    Create ArmamentoCritico.cs

---
+++ Misiones/ArmamentoCritico.cs
@@ -0,0 +1,7 @@
+using System;
+
+public class ArmamentoCritico
+{
+    public string Titulo => "Armamento Crítico";
+    public string Descripcion => "Abdullah y tres compañeros viajan a una armería abandonada para obtener munición esencial para la supervivencia de la Resistencia.";
+}
