e457faeb32672edbe26e383b1ffccddb2a8dbbc1
ExistedAura7234 <162326727+ExistedAura7234@users.noreply.github.com>
2025-10-08 20:40:12 -0300

    Create CazadorDeMaquinas.cs

---
+++ Misiones/CazadorDeMaquinas.cs
@@ -0,0 +1,7 @@
+using System;
+
+public class CazadorDeMaquinas
+{
+    public string Titulo => "Cazador de Máquinas";
+    public string Descripcion => "Como última prueba, Abdullah debe localizar y destruir diez robots que están causando estragos cerca de los refugios de la Resistencia.";
+}
