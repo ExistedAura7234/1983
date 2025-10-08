7440d90d0867da8c8340c1329fb57a8dddbff6d5
ExistedAura7234 <162326727+ExistedAura7234@users.noreply.github.com>
2025-10-08 20:42:22 -0300

    Create AccesoTerminal.cs

---
+++ Misiones/AccesoTerminal.cs
@@ -0,0 +1,7 @@
+using System;
+
+public class AccesoTerminal
+{
+    public string Titulo => "Acceso Terminal";
+    public string Descripcion => "Abdullah se infiltra en las oficinas de la OMS, accede al terminal y consigue los datos críticos para la rebelión.";
+}
