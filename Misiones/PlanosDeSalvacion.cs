e3fb5058917969d39f4a109946cbcf84e002b750
ExistedAura7234 <162326727+ExistedAura7234@users.noreply.github.com>
2025-10-08 20:42:04 -0300

    Create PlanosDeSalvacion.cs

---
+++ Misiones/PlanosDeSalvacion.cs
@@ -0,0 +1,7 @@
+using System;
+
+public class PlanosDeSalvacion
+{
+    public string Titulo => "Planos de Salvación";
+    public string Descripcion => "Tras la victoria, Abdullah contacta a los DevOs, quienes le piden recuperar los planos del A.H.S de la terminal de la OMS.";
+}
