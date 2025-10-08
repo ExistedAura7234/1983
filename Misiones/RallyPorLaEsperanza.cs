92d585ad262a2c92bdde6af0ef75f581e8e30819
ExistedAura7234 <162326727+ExistedAura7234@users.noreply.github.com>
2025-10-08 20:41:43 -0300

    Create RallyPorLaEsperanza.cs

---
+++ Misiones/RallyPorLaEsperanza.cs
@@ -0,0 +1,7 @@
+using System;
+
+public class RallyPorLaEsperanza
+{
+    public string Titulo => "Rally por la Esperanza";
+    public string Descripcion => "Para sellar la alianza con el Escuadrón Tesla, Abdullah debe participar y ganar una carrera en el E-Rally.";
+}
