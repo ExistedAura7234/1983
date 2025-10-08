87f1aaff609030a54bec3e5879154beb55f3f711
ExistedAura7234 <162326727+ExistedAura7234@users.noreply.github.com>
2025-10-08 20:41:02 -0300

    Create AlianzasNecesarias.cs

---
+++ Misiones/AlianzasNecesarias.cs
@@ -0,0 +1,7 @@
+using System;
+
+public class AlianzasNecesarias
+{
+    public string Titulo => "Alianzas Necesarias";
+    public string Descripcion => "Abdullah debe negociar y formar una alianza con las otras organizaciones de sobrevivientes dentro de la ciudad.";
+}
