f487975a19c9f7388e7f27b030a7532fd3722712
ExistedAura7234 <162326727+ExistedAura7234@users.noreply.github.com>
2025-10-08 20:43:47 -0300

    Create RecoleccionVital.cs

---
+++ Misiones/RecoleccionVital.cs
@@ -0,0 +1,7 @@
+using System;
+
+public class RecoleccionVital
+{
+    public string Titulo => "Recolección Vital";
+    public string Descripcion => "Es momento de reunir los materiales necesarios: 10 partes electrónicas, 20 piezas mecánicas y el código de activación de la ONU.";
+}
