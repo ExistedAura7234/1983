d8b9ada65fc2d5af8aa4db756b3fbcd90f365b23
ExistedAura7234 <162326727+ExistedAura7234@users.noreply.github.com>
2025-10-08 20:43:25 -0300

    Create ExodoEstrategico.cs

---
+++ Misiones/ExodoEstrategico.cs
@@ -0,0 +1,7 @@
+using System;
+
+public class ExodoEstrategico
+{
+    public string Titulo => "Éxodo Estratégico";
+    public string Descripcion => "Debido al peligro inminente, la Resistencia debe abandonar y trasladar su cuartel general a una ubicación más segura.";
+}
