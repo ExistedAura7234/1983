c72c11153c428db15a0335d64292f1fd6d0a6054
ExistedAura7234 <162326727+ExistedAura7234@users.noreply.github.com>
2025-10-08 20:38:33 -0300

    Create InicioDeLaResistencia.cs

---
+++ Misiones/InicioDeLaResistencia.cs
@@ -0,0 +1,7 @@
+using System;
+
+public class InicioDeLaResistencia
+{
+    public string Titulo => "Inicio de la Resistencia";
+    public string Descripcion => "Abdullah llega al cuartel general de la Resistencia y se une a ellos. Debe demostrar su valía para ser aceptado entre sus filas.";
+}
